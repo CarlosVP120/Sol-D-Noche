98715b978113e1d9f8362241c09824f012a74276
Diego <diegopartidaromero@gmail.com>
2023-07-01 01:20:59 -0600

    Add about section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import About from "./Pages/About";
 import Home from "./Pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <About />
     </div>
   );
 }
---
+++ src/Pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="flex flex-col items-center justify-center my-8">
+      <h1 className="text-4xl font-bold text-yellow-500 mb-4">About Us</h1>
+      <p className="text-lg text-gray-600">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan
+        nulla non erat luctus, ac eleifend ipsum commodo. Donec fringilla
+        malesuada metus nec fringilla.
+      </p>
+    </div>
+  );
+};
+
+export default About;
