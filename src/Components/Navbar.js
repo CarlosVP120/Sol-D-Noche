9cf5dc26516de1c803986c4ec23fe8c005f3b165
Diego <diegopartidaromero@gmail.com>
2023-07-01 01:09:43 -0600

    Edit the navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,31 +2,30 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-center px-4 py-2 mt-8">
-      {/* <div className="text-2xl font-bold text-stone-300">Sol D Noche</div> */}
-      <ul className="flex gap-4 text-xl text-stone-300">
+    <div className="flex items-center justify-center px-4 py-2 mt-8 min-w-screen">
+      <ul className="flex gap-4 text-xl text-stone-400 font-bold">
         <li>
-          <a href="#" className="p-2 hover:font-semibold ">
+          <a href="#" className="p-2 hover:text-stone-100">
             Home
           </a>
         </li>
         <li>
-          <a href="#" className="p-2 hover:font-semibold">
+          <a href="#" className="p-2 hover:text-stone-100">
             About
           </a>
         </li>
         <li>
-          <a href="#" className="p-2 hover:font-semibold">
+          <a href="#" className="p-2 hover:text-stone-100">
             Services
           </a>
         </li>
         <li>
-          <a href="#" className="p-2 hover:font-semibold">
+          <a href="#" className="p-2 hover:text-stone-100">
             Contact
           </a>
         </li>
       </ul>
-    </nav>
+    </div>
   );
 };
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -16,11 +16,11 @@ const Home = () => {
       >
         <source src="/videos/sea1.mp4" type="video/mp4" />
       </video>
-      <div className="absolute w-screen h-screen bg-black bg-opacity-50 flex flex-col justify-center items-center">
+      <div className="absolute w-screen h-screen bg-black bg-opacity-30 flex flex-col justify-center items-center">
         <h1 className="text-8xl font-bold text-white mb-4">
           Sol <span className="text-yellow-500">D</span> Noche
         </h1>
-        <p className="text-2xl text-white mb-8">
+        <p className="text-3xl text-white mb-8">
           Welcome to <span className="font-semibold">Sol D Noche</span>. Explore
           the magic of day and night by the beach.
         </p>
