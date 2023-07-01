ebdcc6503c6b0b201be47d7fa20658f208b85287
Diego <diegopartidaromero@gmail.com>
2023-07-01 11:32:18 -0600

    Add images in the about section

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,11 +4,16 @@ const About = () => {
   return (
     <div className="flex flex-col items-center justify-center my-8" id="about">
       <h1 className="text-4xl font-bold text-yellow-500 mb-4">About Us</h1>
-      <p className="text-lg text-gray-600">
+      <p className="text-xl text-gray-600 w-1/2 text-center">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan
         nulla non erat luctus, ac eleifend ipsum commodo. Donec fringilla
         malesuada metus nec fringilla.
       </p>
+      <div className="flex justify-center mt-4 gap-8">
+        <img src="images/image1.png" alt="About" className="w-1/5 rounded-md" />
+        <img src="images/image2.png" alt="About" className="w-1/5 rounded-md" />
+        <img src="images/image3.png" alt="About" className="w-1/5 rounded-md" />
+      </div>
     </div>
   );
 };
