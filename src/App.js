a3c1abe4a0fc739b05865945b166040a5c53e50d
Diego <diegopartidaromero@gmail.com>
2023-07-01 23:56:06 -0600

    Replace navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import ScrollableContent from "./Components/ScrollableContent";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 
 function App() {
   return (
     <div className="App">
-      <ScrollableContent />
       <Home />
       <About />
     </div>
