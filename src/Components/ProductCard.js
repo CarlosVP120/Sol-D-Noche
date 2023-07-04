b448422a3d6e172ae585b568c574274489651b63
Diego <diegopartidaromero@gmail.com>
2023-07-04 01:01:26 -0600

    Add some products

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const ProductCard = ({ product }) => {
   return (
     <div className="rounded-lg m-4 shadow-md p-2 hover:shadow-lg transition-all duration-300 transform hover:scale-105">
-      <img src={product.image} alt={product.name} className="w-full md:h-96" />
+      <img src={product.image} alt={product.name} className="w-full h-96" />
       <div className="flex flex-row mt-1 justify-between items-center">
         <h2 className="text-gray-600">{product.name}</h2>
         <p className="text-gray-600">{product.price} MXN</p>
