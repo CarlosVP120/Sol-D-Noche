09444355087dafc351e882d26a4af31634dc81ba
CarlosVP120 <carlosveryan4@gmail.com>
2023-07-05 01:32:25 -0600

    new db

--- src/Firebase/firebase-config.js
+++ src/Firebase/firebase-config.js
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArScF9bA2hJr3Ln9Jz_cO8o8tmWHvNNxo",
-  authDomain: "soldnochewebsite.firebaseapp.com",
-  projectId: "soldnochewebsite",
-  storageBucket: "soldnochewebsite.appspot.com",
-  messagingSenderId: "129207180134",
-  appId: "1:129207180134:web:d1c32a780afd03e790c6a7",
+  apiKey: "AIzaSyDrga_s55IpL-MGj13NonYLO9OwBp-THHU",
+  authDomain: "sol-d-noche.firebaseapp.com",
+  projectId: "sol-d-noche",
+  storageBucket: "sol-d-noche.appspot.com",
+  messagingSenderId: "420274311900",
+  appId: "1:420274311900:web:fb54641e7b6799c121da19",
 };
 
 // Initialize Firebase
