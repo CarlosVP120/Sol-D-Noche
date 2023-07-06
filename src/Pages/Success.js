ac2dc3c10fe750426f104d4ee1a0be376a228e42
Diego <diegopartidaromero@gmail.com>
2023-07-06 00:45:52 -0600

    Change success responsiveness

--- src/Pages/Success.js
+++ src/Pages/Success.js
@@ -3,8 +3,8 @@ import React from "react";
 function Success() {
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-gradient-to-r from-blue-200 to-cyan-200">
-      <div className="flex flex-col justify-center items-center bg-slate-100 p-8 rounded-lg shadow-lg">
-        <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold mb-4 text-gray-800">
+      <div className="flex flex-col justify-center items-center bg-slate-100 p-16 rounded-lg shadow-lg w-3/4 lg:w-1/2">
+        <h1 className="text-4xl sm:text-5xl lg:text-6xl text-center font-bold mb-4 text-gray-800">
           Thanks for your purchase!
         </h1>
         <p className="text-base sm:text-lg lg:text-xl text-stone-600 w-4/5 sm:w-3/5 md:w-1/2 text-center mt-6">
