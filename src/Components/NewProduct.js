d8c12a5ebd97bfb65bdf3615d442d41a5d21a623
Diego <diegopartidaromero@gmail.com>
2023-07-19 14:50:39 -0600

    Dashboard responsive

--- src/Components/NewProduct.js
+++ src/Components/NewProduct.js
@@ -90,145 +90,299 @@ const AddProduct = () => {
   const parse = (val) => val.replace(/^\$/, "");
 
   return (
-    <Box pb={10} animation={appearAnimation}>
-      <Flex alignItems={"center"} p={10}>
-        {/* First letter uppercase */}
-        <Heading color="teal.400">Add Product</Heading>
-      </Flex>
-      <Box w="80%" m="auto" mt="10" border={"0px"} borderRadius="10px" p="5">
-        <Grid templateColumns="repeat(3, 1fr)" gap={6}>
-          <Box my="4">
-            <label htmlFor="name">Name of the product:</label>
-            <Input
-              id="name"
-              placeholder="Name of the product"
-              value={product.name}
-              name="name"
-              onChange={handleChange}
-              mt={1}
-            />
-          </Box>
-          <Box my="4">
-            <label htmlFor="type">Type:</label>
-
-            <Select
-              id="type"
-              placeholder="Seleccionar"
-              value={product.type}
-              name="type"
-              onChange={handleChange}
-              mt={1}
-            >
-              <option value="Beaded Bags">Beaded Bags</option>
-              <option value="Necklaces">Necklaces</option>
-            </Select>
-          </Box>
-          <Box my="4">
-            <label htmlFor="price">Price:</label>
-
-            <NumberInput
-              id="price"
-              onChange={(price) => {
-                setProduct((prev) => ({ ...prev, price: parse(price) }));
-              }}
-              value={format(product.price)}
-              min={0}
-              mt={1}
-            >
-              <NumberInputField />
-            </NumberInput>
-          </Box>
-          <Box my="4">
-            <label htmlFor="availability">Availability:</label>
-
-            <Select
-              id="availability"
-              placeholder="Seleccionar"
-              value={product.availability}
-              name="availability"
-              onChange={handleChange}
-              mt={1}
-            >
-              <option value="Available">Available</option>
-              <option value="Sold">Sold</option>
-            </Select>
-          </Box>
-
-          <Box my="4">
-            <label htmlFor="stoneType">Stone Type:</label>
-            <Input
-              id="stoneType"
-              placeholder="Stone Type"
-              value={product.stoneType}
-              name="stoneType"
-              onChange={handleChange}
-              mt={1}
-            />
-          </Box>
-
-          <Box my="4">
-            <Flex direction="column">
-              <label htmlFor="images">Images:</label>
-              {/* Button to open Modal */}
-              <Button
-                onClick={() => {
-                  setModalOpen(true);
-                }}
-                mt={1}
-              >
-                {product.images.length > 0 ? "Change Images" : "Add Images"}
-              </Button>
-              {/* Modal */}
-              {modalOpen && (
-                <UploadImages
-                  handleImageDrop={handleImageDrop}
-                  modalOpen={modalOpen}
-                  setModalOpen={setModalOpen}
-                  product={product}
-                  setProduct={setProduct}
+    <div>
+      <div className="hidden sm:block">
+        <Box pb={10} animation={appearAnimation}>
+          <Flex alignItems={"center"} p={10}>
+            {/* First letter uppercase */}
+            <Heading color="teal.400">Add Product</Heading>
+          </Flex>
+          <Box
+            w="80%"
+            m="auto"
+            mt="10"
+            border={"0px"}
+            borderRadius="10px"
+            p="5"
+          >
+            <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+              <Box my="4">
+                <label htmlFor="name">Name of the product:</label>
+                <Input
+                  id="name"
+                  placeholder="Name of the product"
+                  value={product.name}
+                  name="name"
+                  onChange={handleChange}
+                  mt={1}
                 />
-              )}
-            </Flex>
-          </Box>
-        </Grid>
-        <Box my="4">
-          <label htmlFor="description">Description:</label>
-          <Textarea
-            id="description"
-            placeholder="Description"
-            value={product.description}
-            name="description"
-            onChange={handleChange}
-            mt={1}
-            mb={3}
-            as={ResizeTextarea}
-          />
-        </Box>
-        <Box my="4">
-          <label htmlFor="stripeID">
-            Stripe ID:{" "}
-            <a
-              href="https://dashboard.stripe.com/test/products/create"
-              className="text-blue-500"
-            >
-              (Get from: https://dashboard.stripe.com/test/products/create)
-            </a>
-          </label>
-          <Input
-            id="stripeID"
-            placeholder="Stripe ID"
-            value={product.stripeID}
-            name="stripeID"
-            onChange={handleChange}
-            mt={1}
-          />
-        </Box>
+              </Box>
+              <Box my="4">
+                <label htmlFor="type">Type:</label>
 
-        <Button onClick={AddProduct} colorScheme="teal">
-          Add Product
-        </Button>
-      </Box>
-    </Box>
+                <Select
+                  id="type"
+                  placeholder="Seleccionar"
+                  value={product.type}
+                  name="type"
+                  onChange={handleChange}
+                  mt={1}
+                >
+                  <option value="Beaded Bags">Beaded Bags</option>
+                  <option value="Necklaces">Necklaces</option>
+                </Select>
+              </Box>
+              <Box my="4">
+                <label htmlFor="price">Price:</label>
+
+                <NumberInput
+                  id="price"
+                  onChange={(price) => {
+                    setProduct((prev) => ({ ...prev, price: parse(price) }));
+                  }}
+                  value={format(product.price)}
+                  min={0}
+                  mt={1}
+                >
+                  <NumberInputField />
+                </NumberInput>
+              </Box>
+              <Box my="4">
+                <label htmlFor="availability">Availability:</label>
+
+                <Select
+                  id="availability"
+                  placeholder="Seleccionar"
+                  value={product.availability}
+                  name="availability"
+                  onChange={handleChange}
+                  mt={1}
+                >
+                  <option value="Available">Available</option>
+                  <option value="Sold">Sold</option>
+                </Select>
+              </Box>
+
+              <Box my="4">
+                <label htmlFor="stoneType">Stone Type:</label>
+                <Input
+                  id="stoneType"
+                  placeholder="Stone Type"
+                  value={product.stoneType}
+                  name="stoneType"
+                  onChange={handleChange}
+                  mt={1}
+                />
+              </Box>
+
+              <Box my="4">
+                <Flex direction="column">
+                  <label htmlFor="images">Images:</label>
+                  {/* Button to open Modal */}
+                  <Button
+                    onClick={() => {
+                      setModalOpen(true);
+                    }}
+                    mt={1}
+                  >
+                    {product.images.length > 0 ? "Change Images" : "Add Images"}
+                  </Button>
+                  {/* Modal */}
+                  {modalOpen && (
+                    <UploadImages
+                      handleImageDrop={handleImageDrop}
+                      modalOpen={modalOpen}
+                      setModalOpen={setModalOpen}
+                      product={product}
+                      setProduct={setProduct}
+                    />
+                  )}
+                </Flex>
+              </Box>
+            </Grid>
+            <Box my="4">
+              <label htmlFor="description">Description:</label>
+              <Textarea
+                id="description"
+                placeholder="Description"
+                value={product.description}
+                name="description"
+                onChange={handleChange}
+                mt={1}
+                mb={3}
+                as={ResizeTextarea}
+              />
+            </Box>
+            <Box my="4">
+              <label htmlFor="stripeID">
+                Stripe ID:{" "}
+                <a
+                  href="https://dashboard.stripe.com/test/products/create"
+                  className="text-blue-500"
+                >
+                  (Get from: https://dashboard.stripe.com/test/products/create)
+                </a>
+              </label>
+              <Input
+                id="stripeID"
+                placeholder="Stripe ID"
+                value={product.stripeID}
+                name="stripeID"
+                onChange={handleChange}
+                mt={1}
+              />
+            </Box>
+
+            <Button onClick={AddProduct} colorScheme="teal">
+              Add Product
+            </Button>
+          </Box>
+        </Box>
+      </div>
+
+      {/* Mobile */}
+      <div className="flex sm:hidden">
+        <Box pb={10} animation={appearAnimation}>
+          <Flex alignItems={"center"} p={2} ml={8}>
+            {/* First letter uppercase */}
+            <Heading color="teal.400">Add Product</Heading>
+          </Flex>
+          <Box w="80%" m="auto" mt="0" border={"0px"} borderRadius="10px" p="0">
+            <Grid templateColumns="repeat(1, 1fr)" gap={6}>
+              <Box my="1">
+                <label htmlFor="name">Name of the product:</label>
+                <Input
+                  id="name"
+                  placeholder="Name of the product"
+                  value={product.name}
+                  name="name"
+                  onChange={handleChange}
+                  mt={1}
+                />
+              </Box>
+              <Box my="1">
+                <label htmlFor="type">Type:</label>
+
+                <Select
+                  id="type"
+                  placeholder="Seleccionar"
+                  value={product.type}
+                  name="type"
+                  onChange={handleChange}
+                  mt={1}
+                >
+                  <option value="Beaded Bags">Beaded Bags</option>
+                  <option value="Necklaces">Necklaces</option>
+                </Select>
+              </Box>
+              <Box my="1">
+                <label htmlFor="price">Price:</label>
+
+                <NumberInput
+                  id="price"
+                  onChange={(price) => {
+                    setProduct((prev) => ({ ...prev, price: parse(price) }));
+                  }}
+                  value={format(product.price)}
+                  min={0}
+                  mt={1}
+                >
+                  <NumberInputField />
+                </NumberInput>
+              </Box>
+              <Box my="1">
+                <label htmlFor="availability">Availability:</label>
+
+                <Select
+                  id="availability"
+                  placeholder="Seleccionar"
+                  value={product.availability}
+                  name="availability"
+                  onChange={handleChange}
+                  mt={1}
+                >
+                  <option value="Available">Available</option>
+                  <option value="Sold">Sold</option>
+                </Select>
+              </Box>
+
+              <Box my="1">
+                <label htmlFor="stoneType">Stone Type:</label>
+                <Input
+                  id="stoneType"
+                  placeholder="Stone Type"
+                  value={product.stoneType}
+                  name="stoneType"
+                  onChange={handleChange}
+                  mt={1}
+                />
+              </Box>
+
+              <Box my="1">
+                <Flex direction="column">
+                  <label htmlFor="images">Images:</label>
+                  {/* Button to open Modal */}
+                  <Button
+                    onClick={() => {
+                      setModalOpen(true);
+                    }}
+                    my={1}
+                  >
+                    {product.images.length > 0 ? "Change Images" : "Add Images"}
+                  </Button>
+                  {/* Modal */}
+                  {modalOpen && (
+                    <UploadImages
+                      handleImageDrop={handleImageDrop}
+                      modalOpen={modalOpen}
+                      setModalOpen={setModalOpen}
+                      product={product}
+                      setProduct={setProduct}
+                    />
+                  )}
+                </Flex>
+              </Box>
+            </Grid>
+            <Box my="4">
+              <label htmlFor="description">Description:</label>
+              <Textarea
+                id="description"
+                placeholder="Description"
+                value={product.description}
+                name="description"
+                onChange={handleChange}
+                mt={1}
+                mb={3}
+                as={ResizeTextarea}
+              />
+            </Box>
+            <Box my="4">
+              <label htmlFor="stripeID">
+                Stripe ID:{" "}
+                <a
+                  href="https://dashboard.stripe.com/test/products/create"
+                  className="text-blue-500"
+                >
+                  (Get from: https://dashboard.stripe.com/test/products/create)
+                </a>
+              </label>
+              <Input
+                id="stripeID"
+                placeholder="Stripe ID"
+                value={product.stripeID}
+                name="stripeID"
+                onChange={handleChange}
+                mt={1}
+              />
+            </Box>
+
+            <Button onClick={AddProduct} colorScheme="teal" mt={2}>
+              Add Product
+            </Button>
+          </Box>
+        </Box>
+      </div>
+    </div>
   );
 };
 
